dd9f3fe271c8c110f6dc66ace738c611ea5ed615
Brayan Lozada <brayanlozada1102@gmail.com>
2026-03-02 10:48:35 -0500

    ajuste 3

--- deposit_logic.py
+++ deposit_logic.py
@@ -1,11 +1,11 @@
-def deposit_logic():
+def deposit_logic(balance):
     
         amount = float(input("Enter the amount to deposit: "))
-        balance = 0
+        
         if amount > 0:
             balance += amount
             print(f"Deposit successful! Your new balance is: {balance}")
         else:
             print("Invalid amount. Please enter a positive number.")
 
-
+    
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from deposit_logic import deposit_logic as dl
+# from deposit_logic import deposit_logic as dl
+# balance = 0.0
+# user_input = input("Enter 'deposit' to make a deposit: ")
+# if user_input == "deposit":
+#  test = True
 
-user_input = input("Enter 'deposit' to make a deposit: ")
-if user_input == "deposit":
- test = True
-
-if test:
-    dl()
+# if test:
+#     dl(balance)
