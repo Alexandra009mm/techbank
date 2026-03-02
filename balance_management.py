98e2293c3bfe36fb20a2c6e85cad522f72569fd2
dilandcv <dilandavidchavez123@gmail.com>
2026-03-02 10:12:07 -0500

    desarrolo de el aumento o disminucio del saldo

--- balance_management.py
+++ balance_management.py
@@ -1,1 +1,17 @@
-print("www")
+saldo = 1000
+
+deposito = int(input("ingrese cuanto desea depositr: ")) 
+saldo_actual = deposito + saldo
+print("usted deposito con exito:", deposito)
+print("su saldo ahora es:", saldo + deposito)
+
+retiro = int(input("ingrese cuanto desea retirar: "))
+
+if retiro <= saldo_actual:
+   print (f"usted ha retirado {retiro} con exito") 
+   print ("su saldo ahora es:", saldo_actual - retiro)
+
+else:
+   print("monto insuficiente")
+   
+
