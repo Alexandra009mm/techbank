f85860d724e0737155d47019eee9575eba40a36b
valncabs <cabasmirandavalen@gmail.com>
2026-03-02 10:01:37 -0500

    agg codigo

---
+++ initial_configuration.py
@@ -0,0 +1,10 @@
+def configurar_sistema():
+    saldo = 1000
+    pin = "1234"
+    limite_intentos = 3
+    intentos = 0
+    historial = []
+
+    print("Bienvenido a TechBank Riwi Digital")
+
+    return saldo, pin, limite_intentos, intentos, historial
