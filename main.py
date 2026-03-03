49d5cf27183cf66b33cc7fca7dea6d3ea942569f
Alexandra009mm <alexaaoerozco@gmail.com>
2026-03-03 08:25:28 -0500

    Add menu display function in main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+def mostrar_menu():
+    print("\n1. Consultar saldo")
+    print("2. Retirar")
+    print("3. Depositar")
+    print("4. Historial")
+    print("5. Salir")
+    return input("Seleccione una opción: ")
