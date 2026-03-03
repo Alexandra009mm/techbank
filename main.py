86b1153a3d136d001e24a51d45eb628cd64b8e18
Brayan Lozada <brayanlozada1102@gmail.com>
2026-03-03 10:56:07 -0500

    ajuste main con return

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from deposit_logic import deposit_logic as dl
-balance = 0.0
-user_input = input("Enter 'deposit' to make a deposit: ")
-if user_input == "deposit":
- test = True
+# saldo = (0.0)
+# user_input = input("Enter 'deposit' to make a deposit: ")
+# if user_input == "deposit":
+#  test = True
 
-if test:
-    dl(balance)
+# if test:
+#     saldo = dl(saldo)
+
+# print(f"su valance actual es {saldo}")
