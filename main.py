a475461b403e8197507422bfa7d7a5abc9e76b0d
Brayan Lozada <brayanlozada1102@gmail.com>
2026-03-03 07:00:02 -0500

    prueba 1

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# from deposit_logic import deposit_logic as dl
-# balance = 0.0
-# user_input = input("Enter 'deposit' to make a deposit: ")
-# if user_input == "deposit":
-#  test = True
+from deposit_logic import deposit_logic as dl
+balance = 0.0
+user_input = input("Enter 'deposit' to make a deposit: ")
+if user_input == "deposit":
+ test = True
 
-# if test:
-#     dl(balance)
+if test:
+    dl(balance)
