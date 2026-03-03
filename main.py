a232cf40a2f9a5f4bb216c43a10b64542fd1d062
alexandra <alexaaoeorzco@gmail.com>
2026-03-03 10:05:21 -0500

    agg cambios codigo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+from deposit_logic import deposit_logic as dl
+
 def mostrar_menu():
     print("\n1. Consultar saldo")
     print("2. Retirar")
@@ -7,23 +7,13 @@ def mostrar_menu():
     print("4. Historial")
     print("5. Salir")
     return input("Seleccione una opción: ")
-from deposit_logic import deposit_logic as dl
+
 balance = 0.0
+
 user_input = input("Enter 'deposit' to make a deposit: ")
 if user_input == "deposit":
  test = True
 
 if test:
     dl(balance)
-=======
-def mostrar_menu():
-    print("\n1. Consultar saldo")
-    print("2. Retirar")
-    print("3. Depositar")
-    print("4. Historial")
-    print("5. Salir")
-    return input("Seleccione una opción: ")
->>>>>>> origin/c3_techbank_helper_functions
-=======
-from initial_configuration import configurar_sistema
->>>>>>> origin/c3_techbank_initial_configuration
+
