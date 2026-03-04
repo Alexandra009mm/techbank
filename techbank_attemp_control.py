b4ff54a40d0b1c4a4d5b13dee551f28137d8281e
sebarebar <sebarebar@gmail.com>
2026-03-04 09:51:35 -0500

    actualizacion codigo final final

---
+++ techbank_attemp_control.py
@@ -0,0 +1,25 @@
+from c3_techbank_authentication import autenticar_usuario
+from techbank_input_num_validation import verificacion_en_bucle
+
+def limites_intentos():
+    attempts = 0 
+
+    while attempts < 3:
+       pin_usuario = verificacion_en_bucle()
+       autenticador = autenticar_usuario(pin_usuario)
+       
+       if autenticador == True:
+           print("Bienvenido al TechBank")
+           return True
+       
+       if autenticador == False: 
+          print("Intento fallido, por favor intnete de nuevo.")
+          attempts += 1
+          if attempts == 3: 
+             print("Limites de intentos alcanzados, intente nuevamente mas tarde")
+             return False
+
+
+
+
+limites_intentos()
