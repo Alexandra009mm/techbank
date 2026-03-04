508ec662be4e19874343ed1eda21aaea843b2648
Luis Reyes <luisrafaelreyescaro1@gmail.com>
2026-03-04 06:26:35 -0500

    commit final

--- c3_techbank_authentication.py
+++ c3_techbank_authentication.py
@@ -1,6 +1,7 @@
 #Esta es una prueba de autenticación
 
-def autenticar_usuario(pin, pin_correcto):
+def autenticar_usuario(pin):
+    pin_correcto = 1234
     if pin != pin_correcto:
         print("Autenticación fallida!")
         return False
