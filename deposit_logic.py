0e387c52015a43148571a872634945f08ddba27b
Brayan Lozada <brayanlozada1102@gmail.com>
2026-03-05 16:11:18 -0500

    actualizacion con configuracion inicial y correcion de errores

--- deposit_logic.py
+++ deposit_logic.py
@@ -1,10 +1,18 @@
-def deposit_logic(saldo, monto):
-        
-        if monto > 0:
-            saldo += monto
-            print(f"Deposito exitoso! Su nuevo saldo es: {saldo}")
-            return saldo
-        else:
-            print("Monto invalido. Porfavor ingresa un valor positivo.")
+import initial_configuration
 
+def deposit_logic():
+        while True:
+            try:
+                monto = float(input("Ingresa el monto a depositar: "))
+        
+                if monto > 0:
+                    initial_configuration.saldo += monto
+                    print(f"Deposito exitoso! Su nuevo saldo es: {initial_configuration.saldo}")
+                    movimiento = f"deposito {monto} / saldo restante: {initial_configuration.saldo}"
+                    initial_configuration.historial.append(movimiento)
+                    return False
+                else:
+                    print("Monto invalido. Porfavor ingresa un valor positivo.")
+            except ValueError:
+                print("Error")
     
