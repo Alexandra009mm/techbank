84a0e5af0101326656cdc579b88b7f0620354926
Valentina Rey Cabas Miranda <142684076+valncabs@users.noreply.github.com>
2026-03-05 06:41:02 -0500

    Refactor initial_configuration.py to remove function call
    Removed the function call and unnecessary comments.

--- initial_configuration.py
+++ initial_configuration.py
@@ -1,5 +1,5 @@
 #configuracion inicial del sistema
-def configurar_sistema():
+
     saldo = 1000
     pin = "1234"
     limite_intentos = 3
@@ -8,8 +8,4 @@ def configurar_sistema():
 
     print("Bienvenido a TechBank Riwi Digital")
 
-    return saldo, pin, limite_intentos, intentos, historial
-
-
-#solo llama la funcion para saber si esta funcionando correctamente. el objetivo es poder inicializar el sistema con los valores predeterminados.
-configurar_sistema()
+  
