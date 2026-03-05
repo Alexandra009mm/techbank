a8d5e49648a39a8d8c3c3c5253bd0731a463f422
kevinpino0609 <kevinpino62@gmail.com>
2026-03-05 09:20:13 -0500

    ajuste

--- interactive_menu.py
+++ interactive_menu.py
@@ -1,29 +1,27 @@
 #kevin pino 
-def mostrar_menu():
-    print("\n1. Consultar saldo")
-    print("2. Retirar")
-    print("3. Depositar")
-    print("4. Historial")
-    print("5. Salir")
-    
+opcion = input("Seleccione una opcion: ")
+
+print("\n1. Consultar saldo")
+print("2. Retirar")
+print("3. Depositar")
+print("4. Historial")
+print("5. Salir")
     # Aquí se pregunta qué operación desea realizar el usuario
-    opcion = input("Seleccione una opción: ")
-
-    if opcion == "1":
-        # Aquí se llama la función consultar_saldo()
-     
-    elif opcion == "2":
-    
-    elif opcion == "3":
+if opcion == "1":
+    # Aquí se llama la función consultar_saldo()
+    pass
+elif opcion == "2":
+    # Aquí se llama la función retirar()
+    pass
+elif opcion == "3":
         # Aquí se llama la función depositar()
-       
-    elif opcion == "4":
-        # Aquí se llama la función historial()
-     
-    elif opcion == "5":
-        # Aquí se llama la función salir() o se termina el programa
-    
-    else:
-        print("Opción inválida. Intente nuevamente.")
+    pass
+elif opcion == "4":
+    # Aquí se llama la función historial()
+    pass
+elif opcion == "5":
+    # Aquí se llama la función salir() o se termina el programa
+    pass
+else:
+    print("Opcion inválida. Intente nuevamente.")
 
-    return opcion
