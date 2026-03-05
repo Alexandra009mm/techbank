b62a8ab29d6febd14fe79d7fb469dc18aae4d423
Joan <estremorjoan@gmail.com>
2026-03-05 11:03:32 -0500

    ajuste rama huerfana

---
+++ c3_techbank_authentication.py
@@ -0,0 +1,12 @@
+#Esta es una prueba de autenticación
+
+def autenticar_usuario(pin):
+    pin_correcto = 1234
+    if pin != pin_correcto:
+        print("Autenticación fallida!")
+        return False
+    
+    if pin == pin_correcto:
+        print("Autenticación exitosa!")
+        return True
+        
---
+++ interactive_menu.py
@@ -0,0 +1,28 @@
+#kevin pino 
+opcion = input("Seleccione una opcion: ")
+
+print("\n1. Consultar saldo")
+print("2. Retirar")
+print("3. Depositar")
+print("4. Historial")
+print("5. Salir")
+    # Aquí se pregunta qué operación desea realizar el usuario
+
+if opcion == "1":
+    # Aquí se llama la función consultar_saldo()
+    pass
+elif opcion == "2":
+    # Aquí se llama la función retirar()
+    pass
+elif opcion == "3":
+        # Aquí se llama la función depositar()
+    pass
+elif opcion == "4":
+    # Aquí se llama la función historial()
+    pass
+elif opcion == "5":
+    # Aquí se llama la función salir() o se termina el programa
+    pass
+else:
+    print("Opcion inválida. Intente nuevamente.")
+
