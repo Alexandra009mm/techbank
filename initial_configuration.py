ce34d7d8def0c265d4836de3e1ef12a3de19634d
Valentina Rey Cabas Miranda <142684076+valncabs@users.noreply.github.com>
2026-03-06 07:26:09 -0500

    Fix initial comment and update welcome message

--- initial_configuration.py
+++ initial_configuration.py
@@ -1,13 +1,24 @@
-#configuracion inicial del sistema
+#configuracion inicial del sistem
 saldo = 1000
 limite_intentos = 3
 intentos = 0
 historial = []
 
+
+
+#--------- BIENVENIDA DEL PROGRAMA ----------
+
 def configurar_sistema():
    
-    print("Bienvenido a TechBank Riwi Digital")
+    mensaje = """
+                    --- Bienvenido a TechBank Riwi Digital ---
+
+           Este sistema es una simulación de un cajero automático
+                desarrollada con fines educativos.
+
+        Permite practicar operaciones bancarias básicas mediante código.
+    """
+    print(mensaje)
 
     
 configurar_sistema()
-  
