421587591cd739d2e2fe68caf2b0f10499ad2f1f
alexandra <edgardo>
2026-03-06 07:09:08 -0500

    mejora diseño 3.0

--- menu.py
+++ menu.py
@@ -7,11 +7,11 @@ import initial_configuration
 
 def mostrar_menu():
     while True:
-        print("\n1. Consultar saldo")
-        print("2. Retirar")
-        print("3. Depositar")
-        print("4. Historial")
-        print("5. Salir del menu")
+        print("\n1. Consultar saldo.")
+        print("2. Retirar.")
+        print("3. Depositar.")
+        print("4. Historial.")
+        print("5. Salir del menu.")
     
         opcion = input("Seleccione una opción: ")
 
