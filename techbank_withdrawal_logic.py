64c830b4e9a966accda63e7495a3f0ba19c96aa5
efrain <hlaluz59@gmail.com>
2026-03-06 06:16:36 -0500

    ajustes

--- techbank_withdrawal_logic.py
+++ techbank_withdrawal_logic.py
@@ -1,15 +1,20 @@
-def logica_retiro(saldo):
-    try:
-        while True:
+import initial_configuration
+
+def logica_retiro():
+    while True:
+        try:
             monto = int(input("ingrese cuanto desea retirar: "))
             if monto < 0:
                  print("Error solo ingrese numeros positivos")
-            elif monto > saldo:
-                print(f"saldo insuficiente, su saldo es {saldo}, por favor ingrese un retiro valido")
+            elif monto > initial_configuration.saldo:
+                print(f"saldo insuficiente, su saldo es {initial_configuration.saldo}, por favor ingrese un retiro valido")
             else:
-                saldo -= monto
-                print(f"proceso exitoso su saldo actual es {saldo}")
-                return saldo
+                initial_configuration.saldo -= monto
+                print(f"proceso exitoso su saldo actual es {initial_configuration.saldo}")
+                movimiento = f"retiro {monto} / saldo restante: {initial_configuration.saldo}"
+                initial_configuration.historial.append(movimiento)
+                return False
+            
 
-    except ValueError:
-      print("Error")
+        except ValueError:
+            print("Error")
