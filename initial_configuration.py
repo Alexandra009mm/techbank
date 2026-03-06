7c4bc5dceede54c4183e4e43157251f9d24d2da7
alexandra <pachecoedgardo2006@gmail.com>
2026-03-06 07:12:49 -0500

    mejora diseño 4.0

--- initial_configuration.py
+++ initial_configuration.py
@@ -4,6 +4,10 @@ limite_intentos = 3
 intentos = 0
 historial = []
 
+
+
+#BIENVENIDA DEL PROGRAMA
+
 def configurar_sistema():
    
     print("---Bienvenido a TechBank Riwi Digital---\n Este sistema es una simulación de un cajero automático desarrollada con fines educativos. \n Permite practicar operaciones bancarias básicas mediante código.")
