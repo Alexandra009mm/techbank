9ad3495b853f6e07a227ab8d7387345f1c130e66
alexandra <pachecoedgardo2006@gmail.com>
2026-03-06 07:24:39 -0500

    cambio mensaje

--- initial_configuration.py
+++ initial_configuration.py
@@ -10,7 +10,15 @@ historial = []
 
 def configurar_sistema():
    
-    print("---Bienvenido a TechBank Riwi Digital---\n Este sistema es una simulación de un cajero automático desarrollada con fines educativos. \n Permite practicar operaciones bancarias básicas mediante código.")
+    mensaje = """
+                    --- Bienvenido a TechBank Riwi Digital ---
+
+           Este sistema es una simulación de un cajero automático
+                desarrollada con fines educativos.
+
+        Permite practicar operaciones bancarias básicas mediante código.
+    """
+    print(mensaje)
 
     
 configurar_sistema()
